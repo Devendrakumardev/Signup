e778825fabf72eee851b2099b7416e93a7043273
Devendra kumar <devendrakumar9580387090@gamil.com>
2024-09-05 08:18:51 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginSign from "./components/LoginSign/LoginSign";
+
+const App = () => {
+  return (
+    <div>
+      <LoginSign />
+    </div>
+  );
+};
+
+export default App;
